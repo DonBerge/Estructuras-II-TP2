47067b57bba53bd1a668c6952ec344aeb3530f5d
DonBerge <agustinfb93@gmail.com>
2024-06-19 23:08:05 -0300

    Un poco de formateo del codigo

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -1,17 +1,18 @@
 module ArrSeq where
+
+import Arr (Arr, (!))
+import Arr qualified as A
 import Par
 import Seq
-import Arr (Arr, (!))
-import qualified Arr as A
 
 instance Seq Arr where
     -- O(1)
     emptyS :: Arr a
     emptyS = A.empty
-    
+
     -- O(1)
     singletonS :: a -> Arr a
-    singletonS = A.fromList . (:[])
+    singletonS = A.fromList . (: [])
 
     -- O(1)
     lengthS :: Arr a -> Int
@@ -29,18 +30,19 @@ instance Seq Arr where
     mapS :: (a -> b) -> Arr a -> Arr b
     mapS f s = A.tabulate (f . (s !)) $ A.length s
 
-    -- W=O(n + sum W f), S = O(lg n + max S f) 
+    -- W=O(n + sum W f), S = O(lg n + max S f)
     filterS :: (a -> Bool) -> Arr a -> Arr a
-    filterS f s = let
-                    f' x
-                        | f x = singletonS x
-                        | otherwise = emptyS
-                  in
-                    A.flatten $ mapS f' s
+    filterS f s =
+        let
+            f' x
+                | f x = singletonS x
+                | otherwise = emptyS
+         in
+            A.flatten $ mapS f' s
 
     -- O(1)
     appendS :: Arr a -> Arr a -> Arr a
-    appendS x y = A.flatten $ A.fromList [x,y]
+    appendS x y = A.flatten $ A.fromList [x, y]
 
     -- O(1)
     takeS :: Arr a -> Int -> Arr a
@@ -53,61 +55,65 @@ instance Seq Arr where
     -- O(1)
     showtS :: Arr a -> TreeView a (Arr a)
     showtS s = case A.length s of
-                0 -> EMPTY
-                1 -> ELT $ s ! 0
-                n -> NODE (takeS s n) (dropS s n)
+        0 -> EMPTY
+        1 -> ELT $ s ! 0
+        n -> NODE (takeS s n) (dropS s n)
 
     -- O(1)
     showlS :: Arr a -> ListView a (Arr a)
     showlS s = case A.length s of
-                0 -> NIL
-                _ -> CONS (s ! 0) (dropS s 1)
+        0 -> NIL
+        _ -> CONS (s ! 0) (dropS s 1)
 
     -- O(n + sum |ai|), O(lg n)
     joinS :: Arr (Arr a) -> Arr a
-    joinS = A.flatten 
+    joinS = A.flatten
 
     reduceS :: (a -> a -> a) -> a -> Arr a -> a
     reduceS f e s = case A.length s of
-                      0 -> e
-                      _ -> f e $ reduce' f s
+        0 -> e
+        _ -> f e $ reduce' f s
       where
         reduce' f s = case A.length s of
-                        1 -> s ! 0
-                        _ -> reduce' f $ compact f s
-        compact f s = let
-                        n = A.length s
-                        apply i
-                              | 2*i+1 == n = s ! (2*i)
-                              | otherwise = f (s ! (2*i)) (s ! (2*i+1))
-                      in
-                        -- Obtengo el techo de la division
-                        A.tabulate apply (div (n + 1) 2)
+            1 -> s ! 0
+            _ -> reduce' f $ compact f s
+        compact f s =
+            let
+                n = A.length s
+                apply i
+                    | 2 * i + 1 == n = s ! (2 * i)
+                    | otherwise = f (s ! (2 * i)) (s ! (2 * i + 1))
+             in
+                -- Obtengo el techo de la division
+                A.tabulate apply (div (n + 1) 2)
 
     scanS :: (a -> a -> a) -> a -> Arr a -> (Arr a, a)
     scanS f b s = case lengthS s of
-                    0 -> (emptyS, b)
-                    1 -> (singletonS b, f b $ s ! 0)
-                    _ -> let
-                            (s',t) = scanS f b $ compact f s
-                         in
-                            (combine f s' s,t)
-        where
-            compact f s = let
-                            n = A.length s
-                            apply i
-                                    | 2*i+1 == n = s ! (2*i)
-                                    | otherwise = f (s ! (2*i)) (s ! (2*i+1))
-                          in
-                            -- Obtengo el techo de la division
-                            A.tabulate apply (div (n + 1) 2)
+        0 -> (emptyS, b)
+        1 -> (singletonS b, f b $ s ! 0)
+        _ ->
+            let
+                (s', t) = scanS f b $ compact f s
+             in
+                (combine f s' s, t)
+      where
+        compact f s =
+            let
+                n = A.length s
+                apply i
+                    | 2 * i + 1 == n = s ! (2 * i)
+                    | otherwise = f (s ! (2 * i)) (s ! (2 * i + 1))
+             in
+                -- Obtengo el techo de la division
+                A.tabulate apply (div (n + 1) 2)
 
-            combine f s' s =  let
-                                apply i
-                                    | even i = s' ! div i 2
-                                    | otherwise = f (s' ! div i 2) (s ! (i-1))
-                              in
-                                A.tabulate apply $ A.length s
+        combine f s' s =
+            let
+                apply i
+                    | even i = s' ! div i 2
+                    | otherwise = f (s' ! div i 2) (s ! (i - 1))
+             in
+                A.tabulate apply $ A.length s
 
     fromList :: [a] -> Arr a
     fromList = A.fromList
